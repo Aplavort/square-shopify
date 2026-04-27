b451d7248a562a5c9faa53ee5f5056f656d2a391
Aplavort <aplavort@gmail.com>
2026-04-27 08:28:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
-
+ 
 const app = express();
-
+ 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Routes
-app.use('/api', require('./routes/payment'));
-
+ 
+// Routes - payment.js is in same folder
+app.use('/api', require('./payment'));
+ 
 // Serve checkout page
 app.get('/checkout', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'checkout.html'));
+  res.sendFile(path.join(__dirname, 'checkout.html'));
 });
-
+ 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
